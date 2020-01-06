2f048fd5049e8904dda4a2d7057f99878f315a8b
EstebanEzequiel <estebanezequielceballos@gmail.com>
2020-01-05 23:37:29 -0300

    inicio blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ConocimientosComponent } from './conocimientos/conocimientos.component';
+import { IdiomasComponent } from './idiomas/idiomas.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { PiePagComponent } from './pie-pag/pie-pag.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    ExperienciaComponent,
+    ConocimientosComponent,
+    IdiomasComponent,
+    ProyectosComponent,
+    PiePagComponent
   ],
   imports: [
     BrowserModule
