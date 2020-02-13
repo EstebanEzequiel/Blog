f1517fd8c92120df0eef148bb548b5a39d51500e
estebanezequiel <estebanezequielceballos@gmail.com>
2020-02-13 16:42:19 -0300

    clear file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { ConocimientosComponent } from './conocimientos/conocimientos.component';
 import { IdiomasComponent } from './idiomas/idiomas.component';
-import { ProyectosComponent } from './proyectos/proyectos.component';
 import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
 import { PreEncabezadoComponent } from './pre-encabezado/pre-encabezado.component';
 import { EstudiosComponent } from './estudios/estudios.component';
@@ -18,7 +17,6 @@ import { EstudiosComponent } from './estudios/estudios.component';
     ExperienciaComponent,
     ConocimientosComponent,
     IdiomasComponent,
-    ProyectosComponent,
     PiePaginaComponent,
     PreEncabezadoComponent,
     EstudiosComponent
